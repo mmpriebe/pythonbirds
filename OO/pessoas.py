2ea9e3c3b8ea0b51717845b75160dbc57eee2f9c
mmpriebe <marciano.priebe@gmail.com>
2024-07-16 18:47:38 -0300

    Criados atributos de instância nome e idade

--- OO/pessoas.py
+++ OO/pessoas.py
@@ -1,4 +1,10 @@
 class Pessoa:
+    """Atributos de um objeto, __init__ metodo contrutor
+    da classe.
+    """
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
     """ metodo está sempre vinculado a 1 objeto
     sempre declarar 'self' em java é 'this'
     """
@@ -7,7 +13,11 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Marciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Marciano'
+    print(p.nome)
+    print(p.idade)
