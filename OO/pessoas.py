15fc3020a4d2c831b342f5155d19cb617e913ac5
mmpriebe <marciano.priebe@gmail.com>
2024-07-16 18:54:10 -0300

    Adicionado atributo complexo a clase

--- OO/pessoas.py
+++ OO/pessoas.py
@@ -2,9 +2,10 @@ class Pessoa:
     """Atributos de um objeto, __init__ metodo contrutor
     da classe.
     """
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
     """ metodo está sempre vinculado a 1 objeto
     sempre declarar 'self' em java é 'this'
     """
@@ -13,11 +14,12 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa('Marciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Marciano'
-    print(p.nome)
-    print(p.idade)
+    renzo = Pessoa(nome='Renzo')
+    luciano = Pessoa(renzo, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho)
